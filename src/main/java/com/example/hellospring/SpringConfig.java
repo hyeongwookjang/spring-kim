67ef6244389848be0bcaafbac9173ab0ae13908f
hyeongwookjang <elonj33@gmail.com>
2022-07-07 10:30:41 +0900

    AOP 적용

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.hellospring;
 
+import com.example.hellospring.aop.TimeTraceAop;
 import com.example.hellospring.repository.*;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,11 +29,15 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }
+
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
 //        return new JpaMemberRepository(em);
-
+//    }
 }
